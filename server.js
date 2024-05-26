74d2d70cbf034ea9b1e8db27c08cdcb9b8b49f44
MORE SHRINIVAS BALAJI <142227902+Shinu87@users.noreply.github.com>
2024-05-27 00:51:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 //ROUTES
 app.use("/api/v1/auth", require("./routes/userRoutes"));
 app.use("/api/v1/post", require("./routes/postRoutes"));
-
+app.get("/",(req,res)=>{
+  req.status(200).send({
+    "success":true,
+    "msg":"node server running"
 //PORT
 const PORT = process.env.PORT || 8080;
 
